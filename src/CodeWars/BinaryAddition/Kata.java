1b682474cf13b7d3ea3b23276212333d21800896
parince <konrad.tupta@gmail.com>
2023-03-03 20:27:10 +0100

    Binary Addition

---
+++ src/CodeWars/BinaryAddition/Kata.java
@@ -0,0 +1,13 @@
+// Implement a function that adds two numbers together and returns their sum in binary.
+// The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+
+package CodeWars.BinaryAddition;
+
+public class Kata{
+    public static String binaryAddition(int a, int b){
+        int dec = a + b;
+        String bin = Integer.toBinaryString(dec);
+        return bin;
+    }
+}
