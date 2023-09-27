4bb55ac2f83fb6248f3ba9b16b13070980e50ae4
parince <konrad.tupta@gmail.com>
2023-09-27 20:38:14 +0200

    Valid Palindrome

--- src/LeetCode/ValidPalindrome/ValidPalindromeTest.java
+++ src/LeetCode/ValidPalindrome/ValidPalindromeTest.java
@@ -3,7 +3,6 @@ package LeetCode.ValidPalindrome;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class ValidPalindromeTest {
     @Test
