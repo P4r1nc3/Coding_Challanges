9197bb7c7de9115bd8cbf96dbaccdff5fded6509
parince <konrad.tupta@gmail.com>
2023-09-27 20:36:24 +0200

    Palindrome Number

--- src/LeetCode/PalindromeNumber/PalindromeNumberTest.java
+++ src/LeetCode/PalindromeNumber/PalindromeNumberTest.java
@@ -1,8 +1,8 @@
 package LeetCode.PalindromeNumber;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class PalindromeNumberTest {
     @Test
