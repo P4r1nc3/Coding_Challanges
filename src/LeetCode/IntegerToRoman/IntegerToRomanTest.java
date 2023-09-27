4319cb191a52a31bfe7a98c569e0fba1bc3125cd
parince <konrad.tupta@gmail.com>
2023-09-27 20:34:40 +0200

    Integer To Roman

--- src/LeetCode/IntegerToRoman/IntegerToRomanTest.java
+++ src/LeetCode/IntegerToRoman/IntegerToRomanTest.java
@@ -1,8 +1,8 @@
 package LeetCode.IntegerToRoman;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class IntegerToRomanTest {
     @Test
